dcfc92017685605bd9ba98257d2fa69396c90e74
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2025-01-17 19:22:22 +0000

    added more shits

--- Game.java
+++ Game.java
@@ -15,16 +15,18 @@ public class Game {
 /*NEXT THINGS ARENT RELATED TO SHREK */,"we are assassins our target is our teacher","sir do we have to call you sir","thing is i dont have the kind of name you just give to people","hi i am from the ministry of defence",
 "do the deed and you get ten billion","i am a gifted improviser","tenticles","seventy percent of the moon gone","a three hundred fifty kilometer per hour fastball","his top speed is mach twenty",
 "which one of you rejcts wants this thing","you were comedic relief dude that is big","just a ten caliber nothing special","he even knows the lines","death is an occupational hazard","an anti me bb",
-"rumor christened him the reaper","this is my afternoon snack","tadaomi karasuma","that was one of our m sixties",
+"rumor christened him the reaper","this is my afternoon snack","tadaomi karasuma","that was one of our m sixties","second blade","like a python","do not hate assassination",
 /*NEXT THINGS ARENT RELATED TO ASSASSINATION CLASSROOM */"but my lord is it legal","i will make it legal","he will not be trained","misa called jar jar binks","roger roger","does not compute uhh you are under arrest",
 "we are taking them to coruscant","the ambassadors are jedi knights i beleive","no need to report that to him","i love democracy","negotiations were short",
 /*NEXT THINGS ARENT RELATED TO STAR WARS */"we are the knights of the round table","how do you know she is a witch","are you suggesting coconuts can migrate","she turned me into a newt","tis but a scratch",
 "what are you gonna do bleed on me","we are the knights who say ni","it is the old man from scene twenty four","there are some who call me tim","one rabbit stew coming right up","he aint got shit all over him"
 ,"what is the airspeed velocity of an unladen swallow","holy hand grenade","pie jesu domine dona eis requiem","no singing",
 /*NEXT THINGS ARENT RELATED TO MONTY PYTHON AN THE HOLY GRAIL */"welcome to the j","jungle one do you copy","they should give me a raise as a private","no do not waste it","fm comms work","i am glad the chicken has died for our cause"
-,"everyone outta die someday red","number twenty three",
+,"everyone outta die someday red","number twenty three","you will be stone dead in a few minutes","do me a favour","i do not want to go on the cart","i am thirty-seven i am not old","i thought we were an autonomous colective",
+"we eat ham and jam and spam a lot","shut up","you got bad breath","you are in great peril","scilence foul temptress","i bet you are gay","shut your noise and get that suit on","did you kill all those guards","camelot is very good pig country",
 /*NEXT THINGS ARENT RELATED TO BUSINESS INSIDER JUNGLE SCHOOL VIDEO */"i hurt people","hey look buddy i am an engineer","be polite","be efficient","have a plan to kill everyone you meet","i am heavy weapons guy",
-"who touched my gun","no that would be your mother","he punched out all my blood","piss","what makes me a good demoman","i do not think it is anything to worry about","doctor assisted homicide"};
+"who touched my gun","no that would be your mother","he punched out all my blood","piss","what makes me a good demoman","i do not think it is anything to worry about","doctor assisted homicide","cry some more",
+"im a force o nature","that soldier is a bloody spy","spy around here","last one alive lock the door","do you believe in magic","nice hustle tons of fun"};
     public boolean gamefinished = false;
     public String gamestatus = "playing";
 
